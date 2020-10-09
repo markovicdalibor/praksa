e29036b70db4cd0eb58fcbd14298981f33d236d3
Dalibor Markovic <cccp_100@hotmail.com>
2020-10-09 08:59:40 +0200

    new

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            scriptLoading: 'defer'
+        })
+    ]
+}
