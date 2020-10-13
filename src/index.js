932c6f86680b9ba064c78b603ec3a4c8f1fd559e
Dalibor Markovic <cccp_100@hotmail.com>
2020-10-13 15:33:01 +0200

    complite

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import logo from "../logo.png";
+import logo from "../bbg.png";
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            scriptLoading: 'defer'
-        })
-    ],
-    mode: 'development'
-}
+        plugins: [
+            new CleanWebpackPlugin(),
+            new HtmlWebpackPlugin({
+                template: 'src/index.html',
+                scriptLoading: 'defer'
+            })
+        ],
+        mode: 'development',
+        module: {
+            rules: [{
+                test: /\.(jpg|png)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: { limit: 10000 }
+                },
+                { loader: 'image-webpac-loader' }
+            }]
+        }
