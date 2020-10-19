d543eb5358285af783caa989949456f8fb4ace4d
Dalibor Markovic <cccp_100@hotmail.com>
2020-10-19 06:29:33 +0200

    new webpack

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import logo from "../images/logo.png"; <<
-<< << < HEAD
-import bbg from "../images/bbg.png"; ===
-=== =
-import bbg from "../images/bbg.png"; >>>
->>> > 05 c2a001852bc00f8dfff89fdd028b2d99be6635
--- webpack.config.js
+++ webpack.config.js
@@ -2,21 +2,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-        plugins: [
-            new CleanWebpackPlugin(),
-            new HtmlWebpackPlugin({
-                template: 'src/index.html',
-                scriptLoading: 'defer'
-            })
-        ],
-        mode: 'development',
-        module: {
-            rules: [{
-                test: /\.(jpg|png)$/,
-                use: {
-                    loader: 'url-loader',
-                    options: { limit: 10000 }
-                },
-                { loader: 'image-webpac-loader' }
-            }]
-        }
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            scriptLoading: 'defer'
+        })
+    ],
+    mode:"production"
+}
